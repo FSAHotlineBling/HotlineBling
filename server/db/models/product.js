634489b9c44cd5ae0da3eae82ffbebf3718af65e
BreAnna <bfsilva@uci.edu>
2017-10-31 14:05:04 -0500

    Chance price datatype to decimal

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -11,7 +11,7 @@ const Product = db.define('product', {
     type: Sequelize.TEXT
   },
   price: {
-    type: Sequelize.FLOAT
+    type: Sequelize.DECIMAL(10,2)
   },
   quantityAvailable: {
     type: Sequelize.INTEGER,
