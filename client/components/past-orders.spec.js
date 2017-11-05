ab462ea255b7fce633121787faa3234cb87fd1d8
bfsilva713 <bfsilva@uci.edu>
2017-11-04 19:22:28 -0500

    wrote tests for past-orders

---
+++ client/components/past-orders.spec.js
@@ -0,0 +1,35 @@
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { spy } from 'sinon'
+import { PastOrders } from './past-orders'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('PastOrders component', () => {
+
+  let pastOrders
+
+  let fakeProps = {
+    pastOrders: [{id: 1, dateCreated: '', status: '', total: ''}, {id: 1, dateCreated: '', status: '', total: ''}],
+    fetchUserOrders: () => {},
+    user: {isAdmin: true},
+    userId: 1
+  }
+
+  beforeEach(() => {
+    pastOrders = shallow(<PastOrders {...fakeProps} />)
+  })
+
+  it('should list each of the user\'s past orders', () => {
+    expect(pastOrders.find('ul').first().children().length).to.be.equal(2);
+  })
+})
+
+
+// it('renders the email in an h3', () => {
+//   expect(userHome.find('h3').children().length).to.be.equal(3)
+// })
+// })
